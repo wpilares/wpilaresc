edd76d961308c176bc1f228ca4a076788b78dfc0
wpilares <wgpilaresc@gmail.com>
2023-03-19 22:20:42 -0500

    added vars & header

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,13 +1,21 @@
 import { type ReactElement } from 'react';
 import styles from './Header.module.scss';
+import Image from 'next/image';
 
 const Header = (): ReactElement => {
   return (
     <div className={styles.header}>
+      <Image
+        src="./menu.svg"
+        alt="menu"
+        height={50}
+        width={50}
+        className={styles.menu}
+      />
       <div className={styles.navbarBrand}>
-        <div>W</div>
-        <span>W</span>
-        <span>Pilares</span>
+        <div className={styles.logo}>W</div>
+        <span className={styles.firstTitle}>W</span>
+        <span className={styles.secondTitle}>Pilares</span>
       </div>
       <div className={styles.navbarLinks}>
         <div>Home</div>
