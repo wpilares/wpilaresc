0b73f3b3257c043133d579d66815e04bd026d543
wpilares <wgpilaresc@gmail.com>
2023-03-20 01:08:59 -0500

    test

---
+++ docs/_next/static/bMmcD4oBqJqGW4xDteS0y/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/0213bb0cadd86c64.css","static/chunks/pages/index-de9f0bb47a74508f.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
